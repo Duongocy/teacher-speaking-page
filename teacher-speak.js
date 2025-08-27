50e097162188f27418a3f643e6b94fdadc6df355
Y Duong Ngoc <ydn@hoya.com>
2025-08-27 10:42:23 +0700

    abcd 1

--- teacher-speak.js
+++ teacher-speak.js
@@ -28,7 +28,7 @@ const word_sentence_list = document.getElementById('wordSentenceList');
 const input_grammer = document.getElementById('inputGrammer');
 const input_level = document.getElementById('inputLevel');
 
-const url_api = 'https://learning-english-api-o1yh.onrender.com';
+const url_api = 'https://english-learning-api-gxwm.onrender.com';
 // const url_api = 'http://localhost:3003';
 
 let display_lesson_list_container = true;
